fa83e1043fecc2d312e60b2cba83f301419d4cad
fanolans <mailenolan@gmail.com>
2023-01-17 23:42:39 +0000

    create note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,17 +1,19 @@
+import 'package:flutter/material.dart';
+
 class Note {
   final String id;
-  final String tittle;
+  final String title;
   final String note;
   final DateTime updatedAt;
   final DateTime createdAt;
   bool isPinned;
 
   Note({
-    required this.id,
-    required this.tittle,
-    required this.note,
-    required this.updatedAt,
-    required this.createdAt,
+    @required this.id,
+    @required this.title,
+    @required this.note,
+    @required this.updatedAt,
+    @required this.createdAt,
     this.isPinned = false,
   });
 }
