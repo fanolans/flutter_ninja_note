9bf419dd526d5dcc169a5f4deca084feca2376ff
fanolans <mailenolan@gmail.com>
2023-01-20 21:42:43 +0000

    add copyWith function

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -16,4 +16,22 @@ class Note {
     @required this.createdAt,
     this.isPinned = false,
   });
+
+  Note copyWith({
+    String id,
+    String title,
+    String note,
+    DateTime updatedAt,
+    DateTime createdAt,
+    bool isPinned,
+  }) {
+    return Note(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      note: note ?? this.note,
+      updatedAt: updatedAt ?? this.updatedAt,
+      createdAt: createdAt ?? this.createdAt,
+      isPinned: isPinned ?? this.isPinned,
+    );
+  }
 }
