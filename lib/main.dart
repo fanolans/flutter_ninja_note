9457f6c9b965b666eed55fc4d7ba19d1e4f75211
fanolans <mailenolan@gmail.com>
2023-01-21 23:59:52 +0000

    add changenotifierprovider and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:note/providers/notes_provider.dart';
+import 'package:note/screens/add_or_detail_screen.dart';
 import 'package:note/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: HomeScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => NotesProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        home: HomeScreen(),
+        routes: {
+          AddOrDetailScreen.routeName: (ctx) => AddOrDetailScreen(),
+        },
+      ),
     );
   }
 }
