33f165c137f413874bc58bdb30b464687b9091c9
fanolans <mailenolan@gmail.com>
2023-01-22 00:03:12 +0000

    change to provider

--- lib/widgets/note_item_widget.dart
+++ lib/widgets/note_item_widget.dart
@@ -1,60 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:note/presentations/custom_icons_icons.dart';
+import 'package:note/providers/notes_provider.dart';
+import 'package:note/screens/add_or_detail_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../models/note_model.dart';
 
 class NoteItem extends StatefulWidget {
   // const NoteItem({Key key}) : super(key: key);
   final String id;
-  final String title;
-  final String note;
-  final bool isPinned;
-  final Function(String id) toggleIsPinnedFn;
+
   const NoteItem({
     @required this.id,
-    @required this.note,
-    @required this.title,
-    @required this.isPinned,
-    @required this.toggleIsPinnedFn,
   });
   @override
   State<NoteItem> createState() => _NoteItemState();
 }
 
 class _NoteItemState extends State<NoteItem> {
-  bool _isPinned;
-
   @override
   Widget build(BuildContext context) {
-    _isPinned = widget.isPinned;
-    return GridTile(
-      header: Align(
-        alignment: Alignment.topRight,
-        child: IconButton(
-          onPressed: () {
-            widget.toggleIsPinnedFn(widget.id);
-          },
-          icon: Icon(
-            _isPinned ? CustomIcons.pin : CustomIcons.pin_outline,
+    final notesProvider = Provider.of<NotesProvider>(context, listen: false);
+    Note note = notesProvider.getNote(widget.id);
+    return GestureDetector(
+      onTap: () => Navigator.of(context).pushNamed(
+        AddOrDetailScreen.routeName,
+        arguments: note.id,
+      ),
+      child: GridTile(
+        header: Align(
+          alignment: Alignment.topRight,
+          child: IconButton(
+            onPressed: () {
+              notesProvider.toggleIsPinned(note.id);
+            },
+            icon: Icon(
+              note.isPinned ? CustomIcons.pin : CustomIcons.pin_outline,
+            ),
           ),
         ),
-      ),
-      footer: ClipRRect(
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(10),
-          bottomRight: Radius.circular(10),
+        footer: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+          ),
+          child: GridTileBar(
+            backgroundColor: Colors.black87,
+            title: Text(note.title),
+          ),
         ),
-        child: GridTileBar(
-          backgroundColor: Colors.black87,
-          title: Text(widget.title),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey[800],
+          ),
+          padding: const EdgeInsets.all(12),
+          child: Text(note.note),
         ),
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(),
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.grey[800],
-        ),
-        padding: const EdgeInsets.all(12),
-        child: Text(widget.note),
       ),
     );
   }
--- lib/widgets/notes_grid_widget.dart
+++ lib/widgets/notes_grid_widget.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:note/providers/notes_provider.dart';
 import 'package:note/widgets/note_item_widget.dart';
+import 'package:provider/provider.dart';
 
 import '../models/note_model.dart';
 
 class NotesGrid extends StatefulWidget {
-  final List<Note> lisNote;
-  final Function(String id) toggleIsPinnedFn;
-
-  NotesGrid({this.lisNote, this.toggleIsPinnedFn});
-
   @override
   State<NotesGrid> createState() => _NotesGridState();
 }
@@ -16,21 +13,14 @@ class NotesGrid extends StatefulWidget {
 class _NotesGridState extends State<NotesGrid> {
   @override
   Widget build(BuildContext context) {
-    List<Note> tempListNote =
-        widget.lisNote.where((note) => note.isPinned).toList();
-    tempListNote.addAll(
-      widget.lisNote.where((note) => !note.isPinned).toList(),
-    );
+    final notesProvider = Provider.of<NotesProvider>(context);
+    List<Note> listNote = notesProvider.notes;
     return Padding(
       padding: const EdgeInsets.all(10),
       child: GridView.builder(
-        itemCount: tempListNote.length,
+        itemCount: listNote.length,
         itemBuilder: (ctx, index) => NoteItem(
-          id: tempListNote[index].id,
-          note: tempListNote[index].note,
-          title: tempListNote[index].title,
-          isPinned: tempListNote[index].isPinned,
-          toggleIsPinnedFn: widget.toggleIsPinnedFn,
+          id: listNote[index].id,
         ),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
