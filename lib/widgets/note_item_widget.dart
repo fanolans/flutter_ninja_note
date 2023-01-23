a4adab43cfd2111b13daf166f334259109b22514
fanolans <mailenolan@gmail.com>
2023-01-23 14:25:11 +0000

    add dismissable widget

--- lib/widgets/note_item_widget.dart
+++ lib/widgets/note_item_widget.dart
@@ -9,8 +9,7 @@ import '../models/note_model.dart';
 class NoteItem extends StatefulWidget {
   // const NoteItem({Key key}) : super(key: key);
   final String id;
-
-  const NoteItem({
+  NoteItem({
     @required this.id,
   });
   @override
@@ -22,41 +21,47 @@ class _NoteItemState extends State<NoteItem> {
   Widget build(BuildContext context) {
     final notesProvider = Provider.of<NotesProvider>(context, listen: false);
     Note note = notesProvider.getNote(widget.id);
-    return GestureDetector(
-      onTap: () => Navigator.of(context).pushNamed(
-        AddOrDetailScreen.routeName,
-        arguments: note.id,
-      ),
-      child: GridTile(
-        header: Align(
-          alignment: Alignment.topRight,
-          child: IconButton(
-            onPressed: () {
-              notesProvider.toggleIsPinned(note.id);
-            },
-            icon: Icon(
-              note.isPinned ? CustomIcons.pin : CustomIcons.pin_outline,
+    return Dismissible(
+      key: Key(note.id),
+      onDismissed: (direction) {
+        notesProvider.deleteNote(note.id);
+      },
+      child: GestureDetector(
+        onTap: () => Navigator.of(context).pushNamed(
+          AddOrDetailScreen.routeName,
+          arguments: note.id,
+        ),
+        child: GridTile(
+          header: Align(
+            alignment: Alignment.topRight,
+            child: IconButton(
+              onPressed: () {
+                notesProvider.toggleIsPinned(note.id);
+              },
+              icon: Icon(
+                note.isPinned ? CustomIcons.pin : CustomIcons.pin_outline,
+              ),
             ),
           ),
-        ),
-        footer: ClipRRect(
-          borderRadius: const BorderRadius.only(
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
+          footer: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+            child: GridTileBar(
+              backgroundColor: Colors.black87,
+              title: Text(note.title),
+            ),
           ),
-          child: GridTileBar(
-            backgroundColor: Colors.black87,
-            title: Text(note.title),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(),
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.grey[800],
+            ),
+            padding: const EdgeInsets.all(12),
+            child: Text(note.note),
           ),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(),
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.grey[800],
-          ),
-          padding: const EdgeInsets.all(12),
-          child: Text(note.note),
         ),
       ),
     );
